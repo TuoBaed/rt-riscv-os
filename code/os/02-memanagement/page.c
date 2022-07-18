35cf77e033cfd1d824dbbdbb28ca397f8f4f7f9f
lw <liweieternity@163.com>
2022-07-18 00:14:08 -0700

    comments on page.c

--- code/os/02-memanagement/page.c
+++ code/os/02-memanagement/page.c
@@ -29,6 +29,8 @@ static uint32_t _num_pages = 0;
 #define PAGE_TAKEN (uint8_t)(1 << 0)
 #define PAGE_LAST  (uint8_t)(1 << 1)
 
+
+/****************************************************************************/
 /*
  * Page Descriptor 
  * flags:
@@ -66,7 +68,10 @@ static inline int _is_last(struct Page *page)
 		return 0;
 	}
 }
+/****************************************************************************/
 
+
+/****************************helper fun*************************************/
 /*
  * align the address to the border of page(4K)
  */
@@ -81,7 +86,7 @@ static inline uint32_t _align_page(uint32_t address)
 ** 参数： 地址值
 ** 返回值： 与4K对齐的地址值
 ** 例如：传入0则返回0；传入1-4096则返回4096；传入4097-8192则返回8192
-** 
+** 在任何情况下，两个函数是可以互换的
 */
 static uint32_t _align_page2(uint32_t address) {
     uint32_t quotient = address / 4096;
@@ -100,7 +105,12 @@ void page_init()
 	 * We reserved 8 Page (8 x 4096) to hold the Page structures.
 	 * It should be enough to manage at most 128 MB (8 x 4096 x 4096) 
 	 */
-	_num_pages = (HEAP_SIZE / PAGE_SIZE) - 8;
+	/* 确实，共计128M的物理内存空间，也就是128 * 2^20 Byte，每个Page的大小为4096 Byte
+	** 也就是2^12 Byte,这样128M共含有2^15个page，如果每1page都需要用1个字节来标记使用情况的话，
+	** 那么，就需要2^15 Byte来保存page的使用情况，也就是8个page，实际heap的空间是达不到128M的，
+	** 所以，8个page来标记page的使用情况是足够的
+	*/
+	_num_pages = (HEAP_SIZE / PAGE_SIZE) - 8;	// 可用的heap空间等于总heap空间减去标志page使用情况的8页空间
 	printf("HEAP_START = %x, HEAP_SIZE = %x, num of pages = %d\n", HEAP_START, HEAP_SIZE, _num_pages);
 	
 	struct Page *page = (struct Page *)HEAP_START;
@@ -118,7 +128,10 @@ void page_init()
 	printf("BSS:    0x%x -> 0x%x\n", BSS_START, BSS_END);
 	printf("HEAP:   0x%x -> 0x%x\n", _alloc_start, _alloc_end);
 }
+/****************************************************************************/
 
+
+/****************************************************************************/
 /*
  * Allocate a memory block which is composed of contiguous physical pages
  * - npages: the number of PAGE_SIZE pages to allocate
@@ -190,7 +203,10 @@ void page_free(void *p)
 		}
 	}
 }
+/****************************************************************************/
 
+
+/****************************************************************************/
 void page_test()
 {
 	void *p = page_alloc(2);
@@ -204,4 +220,4 @@ void page_test()
 	void *p3 = page_alloc(4);
 	printf("p3 = 0x%x\n", p3);
 }
-
+/****************************************************************************/
